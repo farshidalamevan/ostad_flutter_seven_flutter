2b2392dac4b7f79a734f06fb8075e61c09afbf74
farshidalamevan <farshid.a.evan@gmail.com>
2024-08-24 16:21:25 +0600

    MediaQuery | OrientationBuilder | Wrap | LayoutBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,257 +1,92 @@
-/// Add Text Theme
+/// MediaQuery | OrientationBuilder | Wrap | LayoutBuilder
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Module9Class2());
+  runApp(Module11Class1());
 }
 
-class Module9Class2 extends StatelessWidget {
-  const Module9Class2({super.key});
+class Module11Class1 extends StatelessWidget {
+  const Module11Class1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Home(),
-      title: 'Intro App',
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.pink,
-          titleTextStyle: TextStyle(
-              color: Colors.white,
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2),
-        ),
-        textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.black,
-                textStyle:
-                    TextStyle(fontSize: 16, fontWeight: FontWeight.w900))),
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(color: Colors.green.shade200),
-          labelStyle: TextStyle(fontSize: 16),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(
-              color: Colors.yellow,
-            ),
-          ),
-          disabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(
-              color: Colors.red,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: Colors.blue, width: 2),
-          ),
-        ),
-
-        /// Text customize by THEME
-        textTheme: TextTheme(
-            titleLarge: TextStyle(
-                fontSize: 32, color: Colors.white, fontWeight: FontWeight.bold),
-            titleMedium: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-                fontWeight: FontWeight.w600)),
-      ),
-
-      /// For Dark Theme
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.green,
-          titleTextStyle: TextStyle(
-              color: Colors.white,
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2),
-        ),
-        textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-                foregroundColor: Colors.black,
-                backgroundColor: Colors.green,
-                textStyle:
-                    TextStyle(fontSize: 16, fontWeight: FontWeight.w900))),
-        inputDecorationTheme: InputDecorationTheme(
-          hintStyle: TextStyle(color: Colors.green.shade200),
-          labelStyle: TextStyle(fontSize: 16),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(
-              color: Colors.yellow,
-            ),
-          ),
-          disabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(
-              color: Colors.red,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: Colors.blue, width: 2),
-          ),
-        ),
-      ),
-      themeMode: ThemeMode.light,
     );
   }
 }
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  TextEditingController _descriptionTextEditingController =
-      TextEditingController();
-
-  @override
   Widget build(BuildContext context) {
+    /// MediaQuery
+    MediaQueryData mediaQuery = MediaQuery.of(context);
+    print(mediaQuery.size);
+    print(mediaQuery.size.aspectRatio);
+    print(mediaQuery.size.flipped);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Home',
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
+        title: Text('Home'),
+        backgroundColor: Colors.grey,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            children: [
-              /// Name
-              TextField(
-                enabled: true,
-                decoration: InputDecoration(
-                  label: Text('Name'),
-                  // labelStyle: TextStyle(fontSize: 16),
-                  icon: Icon(Icons.add),
-                  prefixIcon: Icon(Icons.person),
-                  suffixIcon: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Search',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      )),
-                  hintText: 'Enter Your Name',
-                  // hintStyle: TextStyle(color: Colors.green.shade200),
-                  // border: OutlineInputBorder(
-                  //   borderRadius: BorderRadius.circular(12),
-                  //   borderSide: BorderSide(
-                  //     color: Colors.yellow,
-                  //   ),
-                  // ),
-                  // disabledBorder: OutlineInputBorder(
-                  //   borderRadius: BorderRadius.circular(12),
-                  //   borderSide: BorderSide(
-                  //     color: Colors.red,
-                  //   ),
-                  // ),
-                  // focusedBorder: OutlineInputBorder(
-                  //   borderRadius: BorderRadius.circular(12),
-                  //   borderSide: BorderSide(color: Colors.blue, width: 2),
-                  // ),
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
 
-              /// Password
-              TextField(
-                enabled: true,
-                obscureText: true,
-                decoration: InputDecoration(
-                  label: Text('Password'),
-                  labelStyle: TextStyle(fontSize: 16),
-                  icon: Icon(Icons.password),
-                  hintText: 'Enter Your Password',
-                  hintStyle: TextStyle(color: Colors.green.shade200),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                      color: Colors.yellow,
-                    ),
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              TextField(),
-              SizedBox(
-                height: 16,
-              ),
+      /// OrientationBuilder
+      // body: OrientationBuilder(
+      //   builder: (context, Orientation orientation) {
+      //     if (orientation == Orientation.portrait) {
+      //       return Column(
+      //         children: [
+      //           /// Wrap
+      //           Wrap(
+      //             alignment: WrapAlignment.center,
+      //             crossAxisAlignment: WrapCrossAlignment.center,
+      //             spacing: 16,
+      //             children: [
+      //               Text('1st line testing'),
+      //               Text('2nd line testing'),
+      //               Text('3rd line testing'),
+      //               Text('4rd line testing'),
+      //               Text('5th line testing'),
+      //             ],
+      //           ),
+      //           Text(orientation.name)
+      //         ],
+      //       );
+      //     } else {
+      //       return Text('Too big Screen and Landsscap');
+      //     }
+      //   },
+      // ),
 
-              /// Description
-              TextField(
-                textInputAction: TextInputAction.done,
-                enabled: true,
-                maxLines: 5,
-                maxLength: 100,
-                controller: _descriptionTextEditingController,
-                onTap: () {
-                  print('Tapped on Description Field');
-                },
-                onChanged: (String value) {
-                  print(value);
-                },
-                decoration: InputDecoration(
-                  label: Text('Description'),
-                  alignLabelWithHint: true,
-                  labelStyle: TextStyle(fontSize: 16),
-                  icon: Icon(Icons.description),
-                  hintText: 'Enter Your Description',
-                  hintStyle: TextStyle(color: Colors.green.shade200),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                      color: Colors.yellow,
-                    ),
-                  ),
-                  disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(
-                      color: Colors.red,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  _descriptionTextEditingController.clear();
-                  // _descriptionTextEditingController.text = 'Clear';
-                },
-                child: Text(
-                  'Clear Description',
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-              )
-            ],
-          ),
+      /// LayoutBuilder
+      body: Center(
+        child: LayoutBuilder(
+          builder: (context, BoxConstraints constraints) {
+            if (constraints.maxWidth < 640) {
+              return Text('This is Mobile Apps',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold));
+            } else if (constraints.maxWidth > 640 &&
+                constraints.maxWidth < 1008) {
+              return Text('This is Tablet Apps',
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.pink,
+                      fontWeight: FontWeight.bold));
+            } else {
+              return Text('This is Laptop/Desktop Software',
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold));
+            }
+          },
         ),
       ),
     );
