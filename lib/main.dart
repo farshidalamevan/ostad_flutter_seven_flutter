926c549f6305455988441b20c017d8363f4ef061
farshidalamevan <farshid.a.evan@gmail.com>
2024-08-31 14:31:29 +0600

    Flexible | Expanded | AspectRatio

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-/// FractionallySizedBox
+/// Flexible | Expanded | AspectRatio
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Module11Class2());
+  runApp(Module11Class3());
 }
 
-class Module11Class2 extends StatelessWidget {
-  const Module11Class2({super.key});
+class Module11Class3 extends StatelessWidget {
+  const Module11Class3({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -22,31 +22,65 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    MediaQuery.sizeOf(context).width;
+    MediaQuery.sizeOf(context).height;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         title: Text('Home'),
       ),
-
-      /// FractionallySizedBox
-      body: Container(
-        height: MediaQuery.of(context).size.height,
+      body: SizedBox(
         width: MediaQuery.of(context).size.width,
-        color: Colors.green,
-        child: FractionallySizedBox(
-          heightFactor: 0.5,
-          widthFactor: 0.6,
-          child: Container(
-            color: Colors.yellow,
-            child: Center(
-              child: Text(
-                'FractionallySizedBox',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            /// Flexible
+            // Flexible(
+            //   flex: 2,
+            //   child: Container(
+            //     color: Colors.red,
+            //     width: 200,
+            //   ),
+            // ),
+            // Flexible(
+            //   fit: FlexFit.loose,
+            //   flex: 1,
+            //   child: Container(
+            //     color: Colors.pink,
+            //     height: 100,
+            //     width: 200,
+            //   ),
+            // ),
+
+            /// Expanded (Shorter version of Flexible)
+            // Expanded(
+            //   child: Container(
+            //     color: Colors.pink,
+            //     height: 100,
+            //     width: 200,
+            //   ),
+            // ),
+            // Expanded(
+            //   child: Container(
+            //     color: Colors.orange,
+            //     width: 200,
+            //   ),
+            // ),
+
+            /// AspectRatio
+            // AspectRatio(
+            //   aspectRatio: 5 / 5,
+            //   child: Container(
+            //     color: Colors.pink,
+            //   ),
+            // ),
+            LayoutBuilder(builder: (context, constraints) {
+              return Container(
+                  child: Text(
+                constraints.maxWidth.toString(),
+              ));
+            })
+          ],
         ),
       ),
     );
