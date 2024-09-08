e8888dd8520815dfa79003fe1231fafafb7bd49a
farshidalamevan <farshid.a.evan@gmail.com>
2024-09-08 15:02:20 +0600

    StateFul Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,65 @@
-/// Responsive Design || Responsive Builder || Screen Utils
-
+/// StateFul Widget
 
 import 'package:flutter/material.dart';
-import 'package:ostad_flutter_seven_flutter/home_screen.dart';
 
 void main() {
-  runApp(RSAapp());
+  runApp(Module12Class1());
 }
 
-class RSAapp extends StatelessWidget {
-  const RSAapp({super.key});
+class Module12Class1 extends StatelessWidget {
+  const Module12Class1({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+        backgroundColor: Colors.grey,
+      ),
+      body: Center(
+        child: Text('Center $counter'),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              counter--;
+              setState(() {});
+            },
+            child: Icon(Icons.remove),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              counter++;
+              setState(() {});
+            },
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
     );
   }
 }
